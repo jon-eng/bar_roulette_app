d3352a5c713a44eba270cdfd87800ef8f542714b
Jonathan Eng <jon.kh.eng@gmail.com>
2015-05-07 09:48:21 -0400

    added yelp gem and initializer

---
+++ app/models/bar.rb
@@ -0,0 +1,21 @@
+class Bar
+
+# client = Yelp::Client.new({ consumer_key: ok_CRH_KphYa_fj1moTSgQ,
+#                             consumer_secret: Mf1c3Hr91WPnz41zaQCABjpRDm0,
+#                             token: 2bEYXVE7xxfYM7iTq_HLUclQjCukEEP_,
+#                             token_secret: BV-R6xDwJycZCYzc3fK97k-Pxw8
+#                           })
+
+
+# require 'yelp'
+
+# Yelp.client.configure do |config|
+#   config.consumer_key = Mf1c3Hr91WPnz41zaQCABjpRDm0
+#   config.consumer_secret = Mf1c3Hr91WPnz41zaQCABjpRDm0
+#   config.token = 2bEYXVE7xxfYM7iTq_HLUclQjCukEEP_
+#   config.token_secret = R6xDwJycZCYzc3fK97k
+# end
+
+
+end
+
